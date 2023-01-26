b38956e89ff42019186dc0d42552623976258237
cupokyle <kyleforsythcomedy@gmail.com>
2023-01-26 11:42:17 -0500

    personalize with my apps

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,24 +1,24 @@
 import '../App.scss';
 import React from "react";
 
-const image1 = require("../localimages/fakesite1.png");
-const image2 = require("../localimages/fakesite2.png");
-const image3 = require("../localimages/fakesite3.png");
+const image1 = require("../localimages/clapify.png");
+const image2 = require("../localimages/whatnow.png");
+const image3 = require("../localimages/wedding.png");
 
 
 export default function Projects() {
     return <section className="projectBox">
-        <a href="https://www.lego.com" className="projectItem">
+        <a href="https://cupokyle.github.io/clapify/" className="projectItem">
             <img alt="image1" src={image1} className="projectImage"/>
-            <p className="projectTitle">Website 1</p>
+            <p className="projectTitle">Clapify</p>
         </a>
-        <a href="https://www.lego.com" className="projectItem">
+        <a href="https://wnow.netlify.app/" className="projectItem">
             <img alt="image2" src={image2} className="projectImage"/>
-            <p className="projectTitle">Website 2</p>
+            <p className="projectTitle">What Now?</p>
         </a>
-        <a href="https://www.lego.com" className="projectItem">
+        <a href="https://www.sulitforsyth.ca" className="projectItem">
             <img alt="image3" src={image3} className="projectImage"/>
-            <p className="projectTitle">Website 3</p>
+            <p className="projectTitle">Wedding Website</p>
         </a>
     </section>
 }
