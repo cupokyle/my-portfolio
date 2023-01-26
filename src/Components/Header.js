0b5d6e4ebad3d6f47cbf7e07173c828aee4e89fd
cupokyle <kyleforsythcomedy@gmail.com>
2023-01-25 22:53:31 -0500

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import '../App.css';
+import React from "react";
+
+const appIcon1 = require("../localimages/icon1.png");
+const appIcon2 = require("../localimages/icon2.png");
+const appIcon3 = require("../localimages/icon3.png");
+
+export default function Header() {
+    return <section className="hero">
+        <div className="wordSideBox">
+            I'm just a little guy
+        </div>
+        <div className="portfolioTitle headerItem">Kyle Forsyth's Portfolio</div>
+        <div className="headerSideBox">
+            <a href="www.lego.com"><img src={appIcon1} alt="appIcon" width="40em"/></a>
+            <a href="www.lego.com"><img src={appIcon2} alt="appIcon" width="40em"/></a>
+            <a href="www.lego.com"><img src={appIcon3} alt="appIcon" width="40em"/></a>
+        </div>
+    </section>
+}
---
+++ src/Components/Projects.js
@@ -0,0 +1,24 @@
+import '../App.css';
+import React from "react";
+
+const image1 = require("../localimages/fakesite1.png");
+const image2 = require("../localimages/fakesite2.png");
+const image3 = require("../localimages/fakesite3.png");
+
+
+export default function Projects() {
+    return <section className="projectBox">
+        <a href="https://www.lego.com" className="projectItem">
+            <img alt="image1" src={image1} className="projectImage"/>
+            <p className="projectTitle">Website 1</p>
+        </a>
+        <a href="https://www.lego.com" className="projectItem">
+            <img alt="image2" src={image2} className="projectImage"/>
+            <p className="projectTitle">Website 2</p>
+        </a>
+        <a href="https://www.lego.com" className="projectItem">
+            <img alt="image3" src={image3} className="projectImage"/>
+            <p className="projectTitle">Website 3</p>
+        </a>
+    </section>
+}
