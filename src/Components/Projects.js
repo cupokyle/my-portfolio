acadb3c5f64673b831afa2b9f3ff524ac69d056f
cupokyle <kyleforsythcomedy@gmail.com>
2023-04-10 16:59:45 -0400

    Small updates to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,22 +1,22 @@
 import '../App.scss';
 import React from "react";
 
-const image1 = require("../localimages/clapify.png");
-const image2 = require("../localimages/whatnow.png");
-const image3 = require("../localimages/wedding.png");
+const image1 = require("../localimages/blackjack-screen.png");
+const image2 = require("../localimages/wnow-screen.png");
+const image3 = require("../localimages/weddin-screen.png");
 
 
 export default function Projects() {
     return <section className="projectBox">
-        <a href="https://cupokyle.github.io/clapify/" className="projectItem" target="_blank" rel="noopener noreferrer">
+        <a href="https://rudedudeblackjack.netlify.app/" className="projectItem" target="_blank" rel="noopener noreferrer">
             <img alt="image1" src={image1} className="projectImage"/>
-            <p className="projectTitle">Clapify</p>
+            <p className="projectTitle">Rude Dude Blackjack</p>
         </a>
         <a href="https://wnow.netlify.app/" className="projectItem" target="_blank" rel="noopener noreferrer">
             <img alt="image2" src={image2} className="projectImage"/>
             <p className="projectTitle">What Now?</p>
         </a>
-        <a href="https://www.lego.com" className="projectItem" target="_blank" rel="noopener noreferrer">
+        <a href="https://beautifulwedding.netlify.app/" className="projectItem" target="_blank" rel="noopener noreferrer">
             <img alt="image3" src={image3} className="projectImage"/>
             <p className="projectTitle">Wedding Website</p>
         </a>
